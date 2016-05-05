7c94ab4a0bca9dce41ab8b351348acdde36ad029
pjzaman <pjzaman@ucdavis>
2016-05-05 16:25:44 -0700

    "added color"

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -9,34 +9,27 @@
 
 library(shiny)
 library(ggplot2)
-library(reshape2)
-RiceData<-read.csv("/home/bis180l_local/MEGA/Assignment_4_Anderson.Jordan/RiceSNPData/RiceDiversity.44K.MSU6.Phenotypes.csv",header=T)
-Rice.melt<-melt(RiceData,id.vars="Region")
+
+load("~/MEGAsync/Assignment_4_Zaman.Paul/data_from_SNP_lab.Rdata")
+
+
+data.pheno.mds <- merge(geno.mds,data.pheno,by="row.names",all=T)
 
 # Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
   
-  # Expression that generates a boxplot. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  Rice.melt.ss<-subset(RiceData,variable=input$trait)
-  output$boxPlot <- renderPlot({
+  
+  output$distPlot <- renderPlot({
     
-    # set up the plot
-    pl <- ggplot(data = Rice.melt.ss,
-                 #Use aes_string below so that input$trait is interpreted
-                 #correctly.  The other variables need to be quoted
-                 aes_string(x=input$trait,
-                            y=input$trait,
-                            fill="Region"
-                 )
+    
+    # draw the histogram with the specified number of bins
+    pl<-ggplot(data=data.pheno.mds,
+               aes_string(x=input$Variable1,
+                          y=input$Variable2,
+                          color=input$Color
+               )
     )
+    pl+geom_point()
     
-    # draw the boxplot for the specified trait
-    pl + geom_point()
   })
 })
-
--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -7,34 +7,69 @@
 #    http://shiny.rstudio.com/
 #
 
+
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage( #create the overall page
+shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Iris Data"),
+  titlePanel("Rice Scatter Plot"),
   
-  # Some helpful information
-  helpText("This application creates a boxplot to show difference between",
-           "iris species.  Please use the radio box below to choose a trait",
-           "for plotting"),
+  # Some Directions and Information
+  helpText("Select variables to compare.",
+           "This application will create a scatter plot with options for X and Y axes and color."),
   
-  # Sidebar with a radio box to input which trait will be plotted
+  # Sidebar with a radio box to input which traits will be plotted
   sidebarLayout(
     sidebarPanel(
-      radioButtons("trait", #the input variable that the value will go into
-                   "Choose a trait to display:",
-                   c("Seed.length",
+      radioButtons("Variable1", 
+                   "Choose a variable to display on the X axis:",
+                   c("Alu.Tol",
+                     "Flag.leaf.length",
+                     "Flag.leaf.width",
+                     "Panicle.number.per.plant",
+                     "Plant.height",
+                     "Panicle.length",
+                     "Primary.panicle.branch.number",
+                     "Seed.number.per.panicle",
+                     "Florets.per.panicle",
+                     "Panicle.fertility",
+                     "Seed.length",
                      "Seed.width",
                      "Seed.volume",
-                     "Seed.surface.area")
-      )),
+                     "Seed.surface.area",
+                     "Amylose.content",
+                     "Protein.content")),
+      radioButtons("Variable2", 
+                   "Choose a variable to display on the Y axis:",
+                   c("Alu.Tol",
+                     "Flag.leaf.length",
+                     "Flag.leaf.width",
+                     "Panicle.number.per.plant",
+                     "Plant.height",
+                     "Panicle.length",
+                     "Primary.panicle.branch.number",
+                     "Seed.number.per.panicle",
+                     "Florets.per.panicle",
+                     "Panicle.fertility",
+                     "Seed.length",
+                     "Seed.width",
+                     "Seed.volume",
+                     "Seed.surface.area",
+                     "Amylose.content",
+                     "Protein.content")),
+      radioButtons("Color", 
+                   "Choose a trait to display by color:",
+                   c("Seed.color",
+                     "Pericarp.color",
+                     "Region",
+                     "popID"))
+    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(plotOutput("boxPlot")
+    
+    
+    #Show a plot of the generated distribution
+    mainPanel(plotOutput("distPlot")
     )
   )
 ))
-
-
